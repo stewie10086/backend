9776d4a5efba216bbab0629d01dcc4b9a876942d
5555dfdf <3123048821@qq.com>
2026-03-24 15:05:56 +0800

    3.24处理登录，注册以及登出的token问题

--- src/main/java/org/example/courework3/controller/UserInfoController.java
+++ src/main/java/org/example/courework3/controller/UserInfoController.java
@@ -2,9 +2,11 @@ package org.example.courework3.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.example.courework3.dto.UpdateSelfInfoRequest;
 import org.example.courework3.result.Result;
 import org.example.courework3.result.UserResult;
 import org.example.courework3.service.AuthService;
+import org.example.courework3.service.UpdateInfoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,14 +17,23 @@ public class UserInfoController {
 
     @Autowired
     private AuthService authService;
+    @Autowired
+    private UpdateInfoService updateInfoService;
 
     @GetMapping("/me")
     public Result<UserResult> getSelfInfo(@RequestHeader("Authorization") String authHeader){
         String token = authHeader.replace("Bearer ", "");
         String userId = authService.getUserIdByToken(token);
         UserResult result = new UserResult();
-        result.toDTO(authService.getSelfInfo(userId));
         return Result.success(result.toDTO(authService.getSelfInfo(userId)));
+    }
+
+    @PatchMapping("/me")
+    public Result<UserResult> updateSelfInfo(@RequestHeader("Authorization") String authHeader, @RequestBody UpdateSelfInfoRequest request){
+        String token = authHeader.replace("Bearer ", "");
+        String userId = authService.getUserIdByToken(token);
+        UserResult result = new UserResult();
+        return Result.success(result.toDTO(updateInfoService.updateSelfInfo(userId, request)));
     }
 
 }
---
+++ src/main/java/org/example/courework3/dto/UpdateSelfInfoRequest.java
@@ -0,0 +1,10 @@
+package org.example.courework3.dto;
+
+import lombok.Data;
+
+@Data
+public class UpdateSelfInfoRequest {
+    private String name;
+    private String avatar;
+
+}
