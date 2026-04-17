5139e7474cbd0281b4a56c30d17296b3cde8c439
5555dfdf <3123048821@qq.com>
2026-04-17 13:57:09 +0800

    4.17 前端路由从 History 模式改成 Hash 模式

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useAuthStore } from '@/stores/auth'
 
 import PublicLayout from '@/layouts/PublicLayout.vue'
@@ -112,7 +112,7 @@ const routes = [
 ]
 
 export const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes
 })
 
